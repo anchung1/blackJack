2c216eaae7817dcbeaf0b1a1a5a6c058ed0ac806
Tony Chung <anchung@gmail.com>
2015-10-27 01:10:39 -0400

    room enter/leave

--- App/src/js/components/SampleContainer.js
+++ App/src/js/components/SampleContainer.js
@@ -6,6 +6,8 @@ var store = require('../stores/sampleStore');
 var socket = io();
 
 function setupWebSocket() {
+    /*console.dir(io);
+    console.dir(socket);*/
     socket.on('connect', function() {
         console.log("connected");
     });
@@ -36,12 +38,27 @@ var SampleContainer = React.createClass({
         store.removeSockMsgListener(this._sockCB);
     },
 
-    keypress: function(event) {
+    keypress: function(refId, event) {
+        var ref = {};
+        var wsEvent = '';
+
         if (event.which == 13) {
-            var value = this.refs.input.getDOMNode().value;
+            if (refId == 'input') {
+                ref = this.refs.input;
+                wsEvent = 'event';
+            }
+
+            if (refId == 'gameRoomInput') {
+                ref = this.refs.gameRoomInput;
+                wsEvent = 'room';
+            }
+
+            var value = ref.getDOMNode().value;
+            ref.getDOMNode().select();
+
             /*console.dir(socket);*/
 
-            socket.emit('event', value);
+            socket.emit(wsEvent, value);
         }
     },
 
@@ -54,16 +71,21 @@ var SampleContainer = React.createClass({
     },
 
     render: function(){
-        var msg = <div></div>;
-        if (this.state.holaMsg) {
-            msg = <div>{this.state.holaMsg}</div>;
-        }
+        var msg = <div>{this.state.holaMsg}</div>;
+
+
+        //comment on:
+        //<input type="text" placeholder="Enter Text" ref="input" onKeyPress={this.keypress.bind(this, 'aNode')}/>
+        //this.keypress.bind(this, '1', '2')
+        //above sendds '1' as first arg, '2' as second arg, and last arg is always event
 
         return (
-            <div>
+            <div className="col-xs-offset-1">
                 <h3>Hello World</h3>
-                <input type="text" placeholder="Enter Text" ref="input" onKeyPress={this.keypress}/>
+                <input type="text" placeholder="Enter Text" ref="input" onKeyPress={this.keypress.bind(this, 'input')}/>
                 {msg}
+                <input type='text' placeholder="Game Room Name" ref="gameRoomInput"
+                       onKeyPress={this.keypress.bind(this, 'gameRoomInput')}/>
             </div>
         )
     }
